a4cb60bf1487c6f23442243328a6ee4d29183d7e
tym664 <tdmattson@email.neit.edu>
2025-01-13 10:27:55 -0500

    edited

--- W2D1_textfileHandling.py
+++ W2D1_textfileHandling.py
@@ -1,28 +1,33 @@
 #W2D1 - Text File Handling - Introduction
 
-#Step 1: Import the csv (comma seperated value) libary 
-import csv 
+#STEP 1: import the csv (comma separated value) library
+import csv
 
-total_records = 0 #the total number of records (rows) in the file 
+total_records = 0 #the total number of records (rows) in the file
 
-#conntecting to the files path - swith \to/
-with open ("textFiles/simple.csv") as csvfile: 
-    #indent 1 level! (new block) 
-    # 
-    # 
-    # allow processor to read the file data 
-    file = csv.reader(csvfile) 
-    
-    #loop through every record (row) in the file 
-    for record in file: 
-        
-        
-        total_records += 1 
-        
-    print (f"\nTOTAL RECORDS: {total_records}\n")
+#STEP 2:
+#connecting to the file's path - switch \ to /
+#-----connected to file--------------------------------------------
+print(f"\n{'NAME':10} \t {'NUM':3} \t {'COLOR'}") #HEADER PRINT
+print("------------------------------------")
+with open("text_fileSimple.csv") as csvfile:
+    #indent 1 level! (new block)
 
-        name = record [0]
-        number = record [1]
-        color = record [2] 
-        print (f"{name:10}\t
-               {number:3} \t {color}")
+    #allow processor to read the file data
+    file = csv.reader(csvfile)
+
+    #loop through every record(row) in the file
+    for record in file:
+        #add +1 to total_records
+        total_records += 1
+
+        #print(record) #the list view of each record (row)
+
+        name = record[0]
+        number = record[1]
+        color = record[2]
+
+        print(f"{name:10} \t {number:3} \t {color.title()}")
+#-----disconnected from file--------------------------------------
+print("------------------------------------")
+print(f"\nTOTAL RECORDS: {total_records}\n")
